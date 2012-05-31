6989bbf9fc77b7a7fe48d0dfdda9eb1babc20801
Farhan Khalaf <farhankhalaf@gmail.com>
2012-05-30 22:51:57 -0700

    initialized guard with rspec and cucumber

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,8 +32,9 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
-  config.include Devise::TestHelpers, :type => :controller
-  config.extend ControllerMacros, :type => :controller
+  # TODO: uncomment when devise is installed
+  # config.include Devise::TestHelpers, type: :controller
+  # config.extend ControllerMacros, type: :controller
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :transaction
