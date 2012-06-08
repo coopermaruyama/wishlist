97baa33d9a71852b1a9dd7432ba0b581ab45b4f3
Farhan Khalaf <farhankhalaf@gmail.com>
2012-06-07 20:09:40 -0700

    added user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  validates :first_name, presence: true, length: {within: 2..25}
+  validates :last_name, presence: true, length: {within: 2..30}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) {@user = Factory(:user)}
+
+  it 'should create a new instance given valid attributes' do
+    User.create!(Factory.attributes_for(:user))
+  end
+
+  describe 'validations' do
+
+    it {should validate_presence_of(:first_name)}
+
+    it {should validate_presence_of(:last_name)}
+
+    it {should ensure_length_of(:first_name).is_at_least(2).is_at_most(25)}
+
+    it {should ensure_length_of(:last_name).is_at_least(2).is_at_most(30)}
+
+  end
+
 end
