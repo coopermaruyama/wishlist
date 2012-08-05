46e80180a881508c76c6ee5ab799f9e215211c89
Cooper Maruyama <cooperm@susteen.com>
2012-08-05 16:08:11 -0700

    add public list at /lists/:list_id

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,7 +4,27 @@ class ListsController < ApplicationController
   end
 
   def show
-    respond_with current_user.list
+    @list = List.find(params[:id])
+    @user = User.find(@list.user_id)
+    @line_items = @list.line_items
+    @ids = Array.new
+    @line_items.each do |item|
+      @ids.push(item.product_id)
+    end
+    
+    @result = Amazon::Ecs.item_lookup(@ids.join(','), response_group: 'Medium')
+
+
+    @products = @result.items.map do |item|
+        OpenStruct.new(
+          id: item.get('ASIN'),
+          name: item.get('ItemAttributes/Title'),
+          price: item.get('ItemAttributes/ListPrice/Amount').to_i / 100.0,
+          description: HTMLEntities.new.decode(item.get('EditorialReviews/EditorialReview/Content')),
+          brand: item.get('ItemAttributes/Manufacturer'),
+          hero_img_url: item.get('MediumImage/URL'),
+        )
+      end
   end
 
   def new
