0ec5a13413fd8ee3e8eda995739afb6c5993845a
Cooper Maruyama <cooperm@susteen.com>
2012-08-31 15:23:05 -0700

    resque heroku test

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -6,3 +6,5 @@ uri = URI.parse(ENV["OPENREDIS_URL"])
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
 Dir["#{Rails.root}/app/jobs/*.rb"].each { |file| require file }
+
+Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
