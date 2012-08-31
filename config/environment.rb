cf101189491b258d1bce28f462fcaac58cb77eba
Cooper Maruyama <cooperm@susteen.com>
2012-08-31 15:04:35 -0700

    resque heroku test

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+redis = Redis.connect :url => ENV["OPENREDIS_URL"]
+
 # Initialize the rails application
 Wishlist::Application.initialize!
