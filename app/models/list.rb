35d22d2a8af9182b3d47591c0aac61c0d02b6dab
coopermaruyama <cooperm@susteen.com>
2012-09-16 03:04:19 -0700

    implement challengeBalance method which will check if there is enough balance in the list to buy any product in the list

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,21 @@ class List < ActiveRecord::Base
   attr_accessible :category, :name, :user_id, :balance
   belongs_to :user
   has_many :line_items
+
+  def challengeBalance
+  	eligible = []
+  	self.line_items.each do |item|
+  		result = Amazon::Ecs.item_lookup(item.product_id, response_group: 'Medium').items.first
+  		puts result
+  		price = (result.get('ItemAttributes/ListPrice/Amount').to_i.presence || result.get('OfferSummary/LowestNewPrice/Amount').to_i) / 100.0
+  		if self.balance > price
+  			eligible << item.product_id
+  		end
+  	end
+  	if eligible.present?
+  		return eligible
+  	else
+  		return false
+  	end
+  end
 end
