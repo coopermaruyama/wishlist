7bb87666a3cf7b0b6c247fbd7676d6eae47c4163
coopermaruyama <cooperm@susteen.com>
2012-11-09 18:51:08 -0800

    if a product id is passed back in the payment notification, buy that item

--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -2,6 +2,7 @@ class PaymentNotification < ActiveRecord::Base
 	belongs_to :list
 	serialize :params
 	after_create :add_donation_to_wishlist
+	after_create :check_if_item_bought
 	attr_accessible :params, :list_id, :status, :transaction_id, :amount
 
 	private
@@ -11,5 +12,12 @@ class PaymentNotification < ActiveRecord::Base
 			list.update_attribute(:balance, list.balance + self.amount.to_i)
 			list.challengeBalance #Check if enough has been donated to purchase an item.
 		end
+	end
+
+	def check_if_item_bought
+		#TODO
+		unless self.item_number.blank?
+			list.line_items.find_by_product_id(self.item_number).destroy
+		# buy_item_from_amazon(self.item_number) 
 	end
 end
